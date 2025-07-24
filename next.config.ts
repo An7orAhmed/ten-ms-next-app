2abf21452b360def9be8b9ee7724a84dccd25130
An7orAhmed <antor.mee@gmail.com>
2025-07-25 00:08:38 +0600

    bg changed

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
-    domains: ['cdn.10minuteschool.com', 's3.ap-southeast-1.amazonaws.com', 'png.pngtree.com'],
+    domains: ['cdn.10minuteschool.com', 's3.ap-southeast-1.amazonaws.com', 'img.freepik.com'],
   },
 };
 
