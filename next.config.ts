2c587b3c63dfe1950866127aaaf90994086f7543
An7orAhmed <antor.mee@gmail.com>
2025-07-24 23:20:23 +0600

    img fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', 
+  basePath: '/ten-ms-next-app',
   images: {
+    unoptimized: true,
     domains: ['cdn.10minuteschool.com', 's3.ap-southeast-1.amazonaws.com'],
   },
 };
